9b6f825f77280fa3cb49125389059d160f4fc46d
aplacaba <alacaba@fastmail.com>
2024-06-09 18:08:10 +0800

    Initial checkin

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new s3.Bucket(this, 'MyBucket', { versioned: true });
 
     // The code that defines your stack goes here
 
